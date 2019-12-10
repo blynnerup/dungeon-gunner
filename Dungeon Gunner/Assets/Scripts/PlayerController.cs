e9c67b80e754d34ee96668089a8ea14f4703e0f3
Brian <brian.lynnerup.pedersen@gmail.com>
2019-12-10 10:38:42 +0100

    Inital PlayerController
    Player char now moves and has basic animations.

---
+++ Dungeon Gunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 moveInput;
+    public Rigidbody2D theRb;
+    public Transform gunArm;
+    private Camera theCam;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        // Optimization - never call the camera locally each time. Instantiate it once!
+        theCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        // transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
+        theRb.velocity = moveInput * moveSpeed;
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+        Vector2 offSet = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+
+        if(mousePos.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunArm.localScale = Vector3.one;
+        }
+
+        // Rotate gun arm
+        float angle = Mathf.Atan2(offSet.y, offSet.x) * Mathf.Rad2Deg;
+        gunArm.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if(moveInput != Vector2.zero)
+        {
+            anim.SetBool("isMoving", true);
+        } 
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+    }
+}
