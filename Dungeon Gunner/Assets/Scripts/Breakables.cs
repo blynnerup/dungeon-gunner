3c3999282dc017222a3dcbdbc7324370c3010ea1
blynnerup <brian.lynnerup.pedersen@gmail.com>
2019-12-18 13:38:24 +0100

    Object interactions
    Made object interact in better ways, for better gameplay.

--- Dungeon Gunner/Assets/Scripts/Breakables.cs
+++ Dungeon Gunner/Assets/Scripts/Breakables.cs
@@ -23,37 +23,46 @@ public class Breakables : MonoBehaviour
         
     }
 
+    public void Smash()
+    {
+        Destroy(gameObject);
+
+        AudioManager.instance.PlaySFX(0);
+        // Show broken pieces
+        int numPieces = Random.Range(1, maxPieces);
+
+        for (int i = 0; i < numPieces; i++)
+        {
+            int randomPiece = Random.Range(0, brokenPieces.Length);
+
+            Instantiate(brokenPieces[randomPiece], transform.position, transform.rotation);
+        }
+
+        // Drop items
+        if (shouldDropItem)
+        {
+            float dropChance = Random.Range(0, 100f);
+
+            if (dropChance < itemDropPercent)
+            {
+                int randomItem = Random.Range(0, itemsToDrop.Length);
+                Instantiate(itemsToDrop[randomItem], transform.position, transform.rotation);
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.CompareTag("Player"))
         {
             if(PlayerController.instance.dashCounter > 0)
             {
-                Destroy(gameObject);
-
-                AudioManager.instance.PlaySFX(0);
-                // Show broken pieces
-                int numPieces = Random.Range(1, maxPieces);
-
-                for (int i = 0; i < numPieces; i++)
-                {
-                    int randomPiece = Random.Range(0, brokenPieces.Length);
-
-                    Instantiate(brokenPieces[randomPiece], transform.position, transform.rotation);
-                }
-
-                // Drop items
-                if (shouldDropItem)
-                {
-                    float dropChance = Random.Range(0, 100f);
-
-                    if (dropChance < itemDropPercent)
-                    {
-                        int randomItem = Random.Range(0, itemsToDrop.Length);
-                        Instantiate(itemsToDrop[randomItem], transform.position, transform.rotation);
-                    }
-                }
+                Smash();
             }
+        }
+        if (other.CompareTag("PlayerBullet"))
+        {
+            Smash();
         }
     }
 }
