568a0a6886b5a3680df97880c20e95b88ac053d9
blynnerup <brian.lynnerup.pedersen@gmail.com>
2019-12-23 18:58:26 +0100

    End of prototype

---
+++ Dungeon Gunner/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject layoutRoom;
+    public Color startColor, endColor;
+    public int distanceToEnd;
+
+    public Transform generatorPoint;
+
+    public enum Direction { up, right, down, left};
+    public Direction selectedDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
