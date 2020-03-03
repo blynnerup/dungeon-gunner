c069fed90647986f25e814a056cfeeb6060122b1
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2020-03-03 14:48:01 +0100

    New enemy types
    Created three more enemies for the game. With different mechanics.

--- Dungeon Gunner/Assets/Scripts/EnemyController.cs
+++ Dungeon Gunner/Assets/Scripts/EnemyController.cs
@@ -6,30 +6,51 @@ public class EnemyController : MonoBehaviour
 {
     public Rigidbody2D theRb;
     public float moveSpeed;
-    public Animator anim;
-    public GameObject impactEffect;
 
+    [Header("Chase Player")]
+    public bool shouldChasePlayer;
     public float rangeToChasePlayer;
     private Vector3 moveDirection;
 
-    public int health = 150;
+    [Header("Run Away")]
+    public bool shouldRunAway;
+    public float runAwayRange;
 
-    public GameObject[] deathSplatters;
+    [Header("Wandering")]
+    public bool shouldWander;
+    public float wanderLength, pauseLength;
+    private float wanderCounter, pauseCounter;
+    private Vector3 wanderDirection;
 
+    [Header("Patrolling")]
+    public bool shouldPatrol;
+    public Transform[] patrolPoints;
+    private int currentPatrolPoint;
+
+    [Header("Shooting")]
     public bool shouldShoot;
     public GameObject bullet;
     public Transform firePoint;
     public float fireRate;
     private float fireCounter;
 
+    [Header("Variables")]
+    public Animator anim;
     public SpriteRenderer theBody;
     public float shootRange;
+    public int health = 150;
+    public GameObject[] deathSplatters;
+    public GameObject impactEffect;
 
 
     // Start is called before the first frame update
     void Start()
     {
         theRb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        if (shouldWander)
+        {
+            pauseCounter = Random.Range(pauseLength * .75f, pauseLength * 1.25f);
+        }
     }
 
     // Update is called once per frame
@@ -37,13 +58,57 @@ public class EnemyController : MonoBehaviour
     {
         if (theBody.isVisible && PlayerController.instance.gameObject.activeInHierarchy)
         {
-            if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < rangeToChasePlayer)
+            moveDirection = Vector3.zero;
+
+            if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < rangeToChasePlayer && shouldChasePlayer)
             {
                 moveDirection = PlayerController.instance.transform.position - transform.position;
-            }
-            else
+            } else
             {
-                moveDirection = Vector3.zero;
+                if (shouldWander)
+                {
+                    if (wanderCounter > 0)
+                    {
+                        wanderCounter -= Time.deltaTime;
+
+                        // move the enemy
+                        moveDirection = wanderDirection;
+
+                        if (wanderCounter <= 0)
+                        {
+                            pauseCounter = Random.Range(pauseLength * .75f, pauseLength * 1.25f);
+                        }
+                    }
+
+                    if (pauseCounter > 0)
+                    {
+                        pauseCounter -= Time.deltaTime;
+
+                        if (pauseCounter <= 0)
+                        {
+                            wanderCounter = Random.Range(wanderLength * .75f, wanderLength * 1.25f);
+
+                            wanderDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+                        }
+                    }
+                }
+
+                if (shouldPatrol)
+                {
+                    moveDirection = patrolPoints[currentPatrolPoint].position - transform.position;
+
+                    if (Vector3.Distance(transform.position, patrolPoints[currentPatrolPoint].position) < .2f)
+                    {
+                        currentPatrolPoint++;
+                        if (currentPatrolPoint >= patrolPoints.Length)
+                            currentPatrolPoint = 0;
+                    }
+                }
+            }
+
+            if (shouldRunAway && Vector3.Distance(transform.position, PlayerController.instance.transform.position) < runAwayRange)
+            {
+                moveDirection = transform.position - PlayerController.instance.transform.position;
             }
 
             moveDirection.Normalize();
