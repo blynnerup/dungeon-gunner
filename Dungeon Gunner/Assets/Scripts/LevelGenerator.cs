a8e5aef09e60f2c02a799e8ebd0bfb50e38b9f51
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2020-03-03 14:07:48 +0100

    Procedural Generation
    Rooms are now being generated procedurally. Fun times!

--- Dungeon Gunner/Assets/Scripts/LevelGenerator.cs
+++ Dungeon Gunner/Assets/Scripts/LevelGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelGenerator : MonoBehaviour
 {
@@ -10,18 +11,203 @@ public class LevelGenerator : MonoBehaviour
 
     public Transform generatorPoint;
 
-    public enum Direction { up, right, down, left};
+    public enum Direction { up, right, down, left };
     public Direction selectedDirection;
 
+    public float xOffset = 18;
+    public float yOffset = 10;
+
+    public LayerMask whatIsRoom;
+
+    private GameObject endRoom;
+
+    public RoomPrefab rooms;
+
+    private List<GameObject> generatedOutlines = new List<GameObject>();
+
+    public RoomCenter centerStart, centerEnd;
+    public RoomCenter[] potentialCenters;
+
+    private List<GameObject> layoutRoomObjects = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+
+        selectedDirection = (Direction)Random.Range(0, 4);
+        MoveGenerationPoint();
+
+        for (int i = 0; i < distanceToEnd; i++)
+        {
+            GameObject newRoom = Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation);
+
+            layoutRoomObjects.Add(newRoom);
+
+            if (i+1 == distanceToEnd)
+            {
+                newRoom.GetComponent<SpriteRenderer>().color = endColor;
+                layoutRoomObjects.RemoveAt(distanceToEnd - 1);
+                endRoom = newRoom;
+            }
+
+            selectedDirection = (Direction)Random.Range(0, 4);
+            MoveGenerationPoint();
+
+            while (Physics2D.OverlapCircle(generatorPoint.position, .2f, whatIsRoom))
+            {
+                MoveGenerationPoint();
+            }
+        }
+
+        // Create room outlines
+        CreateRoomOutline(Vector3.zero);
+        foreach (GameObject room in layoutRoomObjects)
+        {
+            CreateRoomOutline(room.transform.position);
+
+        }
+        CreateRoomOutline(endRoom.transform.position);
+
+        foreach (GameObject outline in generatedOutlines)
+        {
+            bool generateCenter = true; 
+
+            if (outline.transform.position == Vector3.zero)
+            {
+                Instantiate(centerStart, outline.transform.position, transform.rotation).theRoom = outline.GetComponent<Room>();
+                generateCenter = false;
+            }
+
+            if (outline.transform.position == endRoom.transform.position)
+            {
+                Instantiate(centerEnd, outline.transform.position, transform.rotation).theRoom = outline.GetComponent<Room>();
+                generateCenter = false;
+            }
+                
+            if (generateCenter)
+            {
+                int centerSelect = Random.Range(0, potentialCenters.Length);
+
+                Instantiate(potentialCenters[centerSelect], outline.transform.position, transform.rotation).theRoom = outline.GetComponent<Room>();
+            }
+
+
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+#if UNITY_EDITOR
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+#endif
     }
+
+    public void MoveGenerationPoint()
+    {
+        switch (selectedDirection)
+        {
+            case Direction.up:
+                generatorPoint.position += new Vector3(0f, yOffset, 0f);
+                break;
+            case Direction.right:
+                generatorPoint.position += new Vector3(xOffset, 0f, 0f);
+                break;
+            case Direction.down:
+                generatorPoint.position += new Vector3(0f, -yOffset, 0f);
+                break;
+            case Direction.left:
+                generatorPoint.position += new Vector3(-xOffset, 0f, 0f);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void CreateRoomOutline(Vector3 roomPosition)
+    {
+        bool roomAbove = Physics2D.OverlapCircle(roomPosition + new Vector3(0f, yOffset, 0f), .2f, whatIsRoom);
+        bool roomBelow = Physics2D.OverlapCircle(roomPosition + new Vector3(0f, -yOffset, 0f), .2f, whatIsRoom);
+        bool roomLeft = Physics2D.OverlapCircle(roomPosition + new Vector3(-xOffset, 0f, 0f), .2f, whatIsRoom);
+        bool roomRight = Physics2D.OverlapCircle(roomPosition + new Vector3(xOffset, 0f, 0f), .2f, whatIsRoom);
+
+        int directionCount = 0;
+        if (roomAbove)
+        {
+            directionCount++;
+        }
+        if (roomBelow)
+        {
+            directionCount++;
+        }
+        if (roomLeft)
+        {
+            directionCount++;
+        }
+        if (roomRight)
+        {
+            directionCount++;
+        }
+
+        switch (directionCount)
+        {
+            case 0:
+                Debug.LogError("Found no room exits!");
+                break;
+            case 1:
+                if (roomAbove)
+                    generatedOutlines.Add(Instantiate(rooms.singleUp, roomPosition, transform.rotation));
+                if (roomRight)
+                    generatedOutlines.Add(Instantiate(rooms.singleRight, roomPosition, transform.rotation));
+                if (roomBelow)
+                    generatedOutlines.Add(Instantiate(rooms.singleDown, roomPosition, transform.rotation));
+                if (roomLeft)
+                    generatedOutlines.Add(Instantiate(rooms.singleLeft, roomPosition, transform.rotation));
+                break;
+            case 2:
+                if (roomAbove && roomRight)
+                    generatedOutlines.Add(Instantiate(rooms.doubleUpRight, roomPosition, transform.rotation));
+                if (roomAbove && roomBelow)
+                    generatedOutlines.Add(Instantiate(rooms.doubleUpDown, roomPosition, transform.rotation));
+                if (roomAbove && roomLeft)
+                    generatedOutlines.Add(Instantiate(rooms.doubleUpLeft, roomPosition, transform.rotation));
+                if (roomBelow && roomRight)
+                    generatedOutlines.Add(Instantiate(rooms.doubleDownRight, roomPosition, transform.rotation));
+                if (roomBelow && roomLeft)
+                    generatedOutlines.Add(Instantiate(rooms.doubleDownLeft, roomPosition, transform.rotation));
+                if (roomLeft && roomRight)
+                    generatedOutlines.Add(Instantiate(rooms.doubleLeftRight, roomPosition, transform.rotation));
+                break;
+            case 3:
+                if (roomAbove && roomRight && roomBelow)
+                    generatedOutlines.Add(Instantiate(rooms.tripleUpRightDown, roomPosition, transform.rotation));
+                if (roomAbove && roomRight && roomLeft)
+                    generatedOutlines.Add(Instantiate(rooms.tripleLeftUpRight, roomPosition, transform.rotation));
+                if (roomAbove && roomBelow && roomLeft)
+                    generatedOutlines.Add(Instantiate(rooms.tripleDownLeftUp, roomPosition, transform.rotation));
+                if (roomRight && roomLeft && roomBelow)
+                    generatedOutlines.Add(Instantiate(rooms.tripleRightDownLeft, roomPosition, transform.rotation));
+                break;
+            case 4:
+                if (roomAbove && roomRight && roomBelow && roomLeft)
+                    generatedOutlines.Add(Instantiate(rooms.fourway, roomPosition, transform.rotation));
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+[System.Serializable]
+public class RoomPrefab
+{
+    public GameObject singleUp, singleRight, singleDown, singleLeft,
+        doubleUpDown, doubleLeftRight, doubleUpRight, doubleUpLeft, doubleDownRight, doubleDownLeft,
+        tripleUpRightDown, tripleRightDownLeft, tripleDownLeftUp, tripleLeftUpRight,
+        fourway;
 }
--- Dungeon Gunner/Assets/Scripts/Room.cs
+++ Dungeon Gunner/Assets/Scripts/Room.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    public bool closeWhenEntered, openWhenEnemiesCleared;
+    public bool closeWhenEntered;
 
     public GameObject[] doors;
 
-    private bool roomActive;
+    [HideInInspector]
+    public bool roomActive;
 
-    public List<GameObject> enemies = new List<GameObject>();
+   
 
     // Start is called before the first frame update
     void Start()
@@ -21,27 +22,37 @@ public class Room : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (enemies.Count > 0 && roomActive)
-        {
-            for (int i = 0; i < enemies.Count; i++)
-            {
-                if (enemies[i] == null)
-                {
-                    enemies.RemoveAt(i);
-                    i--;
-                }
-            }
+        //if (enemies.Count > 0 && roomActive)
+        //{
+        //    for (int i = 0; i < enemies.Count; i++)
+        //    {
+        //        if (enemies[i] == null)
+        //        {
+        //            enemies.RemoveAt(i);
+        //            i--;
+        //        }
+        //    }
 
-            if (enemies.Count == 0)
-            {
-                foreach(GameObject door in doors)
+        //    if (enemies.Count == 0)
+        //    {
+        //        foreach(GameObject door in doors)
+        //        {
+        //            door.SetActive(false);
+
+        //            closeWhenEntered = false;
+        //        } 
+        //    }
+        //}
+    }
+
+    public void OpenDoors()
+    {
+        foreach(GameObject door in doors)
                 {
                     door.SetActive(false);
 
                     closeWhenEntered = false;
                 } 
-            }
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
