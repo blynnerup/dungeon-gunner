da18d486822065565b90722bd542b2f055093828
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2020-03-05 15:18:46 +0100

    Shop Created
    Basic shop health implemented.

---
+++ Dungeon Gunner/Assets/Scripts/ShopItem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : MonoBehaviour
+{
+    public GameObject buyMessage;
+
+    private bool inBuyZone;
+
+    public bool isHealthRestore, isHealthUpgrade, isWeapon;
+    public int itemCost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inBuyZone)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (LevelManager.instance.currentCoins >= itemCost)
+                {
+                    LevelManager.instance.SpendCoins(itemCost);
+
+                    if (isHealthRestore)
+                    {
+                        PlayerHealthController.instance.HealPlayer(PlayerHealthController.instance.maxHealth);
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            buyMessage.SetActive(true);
+            inBuyZone = true;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            buyMessage.SetActive(false);
+            inBuyZone = false;
+        }
+    }
+}
